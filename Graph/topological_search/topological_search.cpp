432f153cbfb72811b25cd9f428bf709dc3b537ef
r1407p <buszter0403@gmail.com>
2023-08-20 19:17:46 +0800

    feat: topolodical sort

--- Graph/topological_search/topological_search.cpp
+++ Graph/topological_search/topological_search.cpp
@@ -28,18 +28,13 @@ int main() {
     };
     n = 5;
 	cycle = false; 
-
-	// 進行DFS
 	for (int i=0; i<n; ++i){
 		if (!visit[i])
 			DFS(i);
     }
-	// 輸出結果
 	if (cycle)
 		cout << "圖上有環";
 	else
-		// 印出一個合理的排列順序
-		// 離開點的順序，頭尾顛倒之後，就是拓撲順序。
 		for (int i=n-1; i>=0; --i)
 			cout << order[i];
 }
