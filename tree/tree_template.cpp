8a566cfe1c72c7703508744b42467ad74d3aae5b
r1407p <buszter0403@gmail.com>
2023-08-21 09:40:10 +0800

    feat tree/bridge

--- tree/tree_template.cpp
+++ tree/tree_template.cpp
@@ -1,8 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n = 9;
+const int n = 9;
 vector<vector<int>> graph; 
+vector<int> visit(n,0);
+int t = 0;
+void dfs(int x,int parent){
 
+}
 int main(){
     graph = {
         {1,2},
@@ -15,5 +19,8 @@ int main(){
         {8},
         {4},
     };
-
+    for(int i =0;i<9;i++){
+        if(!visit[i])
+            dfs(i,-1);
+    }
 }
