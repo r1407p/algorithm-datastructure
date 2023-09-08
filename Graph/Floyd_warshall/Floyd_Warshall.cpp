be4b6addd02c4736ab3c419ea9a33d8c7cc994e0
r1407p <buszter0403@gmail.com>
2023-09-09 03:05:17 +0800

    segment tree

--- Graph/Floyd_warshall/Floyd_Warshall.cpp
+++ Graph/Floyd_warshall/Floyd_Warshall.cpp
@@ -6,22 +6,18 @@ vector<vector<int>> dis(maxn,vector<int>(maxn,9999999));
 vector<vector<int>> mid(maxn,vector<int>(maxn,-1));
 vector<vector<pair<int,int>>> graph;
 
-void floyd_warshall(int n )
-{
+void floyd_warshall(int n ){ // n is n nodes
 	for(int i =0;i<n;i++){
         for(auto path:graph[i]){
             dis[i][path.second] = path.first;
         }
     }
-
 	for (int i=0; i<n; i++)
 		dis[i][i] = 0;
-
 	for (int k=0; k<n; k++){
 		for (int i=0; i<n; i++){
 			for (int j=0; j<n; j++){
-				if (dis[i][k] + dis[k][j] < dis[i][j] || dis[i][j]==-1)
-				{
+				if (dis[i][k] + dis[k][j] < dis[i][j] || dis[i][j]==-1){
 					dis[i][j] = dis[i][k] + dis[k][j];
 					mid[i][j] = k;	// 由i點走到j點經過了k點
 				}
@@ -29,10 +25,8 @@ void floyd_warshall(int n )
         }
     }
 }
-void find_path(int s, int t)		// 印出最短路徑
-{
+void find_path(int s, int t){		// 印出最短路徑
 	if (mid[s][t] == -1) return;	// 沒有中繼點就結束
-
 	find_path(s, mid[s][t]);		// 前半段最短路徑
 	cout << mid[s][t];			// 中繼點
 	find_path(mid[s][t], t);		// 後半段最短路徑
--- segment-tree/segment-tree-simple-add.cpp
+++ segment-tree/segment-tree-simple-add.cpp
@@ -7,57 +7,57 @@ struct node{
     int value;
 };
 vector<node> segment_tree;
-void build(int left,int right,int x ,vector<int> & nums){
-    segment_tree[x].left = left;
-    segment_tree[x].right = right;
-    // cout <<left <<" "<<right<<" "<<x<<endl;
+void build(int left,int right,int index ,vector<int> & nums){
+    segment_tree[index].left = left;
+    segment_tree[index].right = right;
+    // cout <<left <<" "<<right<<" "<<index<<endl;
     if(left == right){ // here is leaf
-        segment_tree[x].value = nums[left];
+        segment_tree[index].value = nums[left];
         return;
     }
     int mid = (left+right)/2;
-    build(left,mid,x<<1,nums);
-    build(mid+1,right,x<<1|1,nums);
-    segment_tree[x].value = segment_tree[x<<1].value+segment_tree[x<<1|1].value;
+    build(left,mid,index<<1,nums);
+    build(mid+1,right,index<<1|1,nums);
+    segment_tree[index].value = segment_tree[index<<1].value+segment_tree[index<<1|1].value;
 }
-void modify(int position ,int x,int value){
-    if(segment_tree[x].left == position && segment_tree[x].right ==position){ // here is leaf
-        segment_tree[x].value = value;
+void modify(int position ,int index,int value){
+    if(segment_tree[index].left == position && segment_tree[index].right ==position){ // here is leaf
+        segment_tree[index].value = value;
         return;
     }
-    int mid = (segment_tree[x].left+segment_tree[x].right)/2;
+    int mid = (segment_tree[index].left+segment_tree[index].right)/2;
     
     if(position<=mid){
-        modify(position,x<<1,value);
+        modify(position,index<<1,value);
     }else{
-        modify(position,x<<1|1,value);
+        modify(position,index<<1|1,value);
     }
-    segment_tree[x].value = segment_tree[x<<1].value+segment_tree[x<<1|1].value;
+    segment_tree[index].value = segment_tree[index<<1].value,segment_tree[index<<1|1].value;
 }
-int query(int i,int j,int x){
-    // cout <<i <<" "<<j <<" "<<segment_tree[x].left<<" " <<segment_tree[x].right<<endl;
+int query(int i,int j,int index){
+    // cout <<i <<" "<<j <<" "<<segment_tree[index].left<<" " <<segment_tree[index].right<<endl;
     int res = 0;
-    int left = segment_tree[x].left;
-    int right = segment_tree[x].right;
+    int left = segment_tree[index].left;
+    int right = segment_tree[index].right;
     int mid = (left+right)/2;
-    if(segment_tree[x].left==i && segment_tree[x].right ==j){
+    if(segment_tree[index].left==i && segment_tree[index].right ==j){
         
-        return segment_tree[x].value;
+        return segment_tree[index].value;
     }
-    if(i>mid)return query(i,j,x*2+1);
-    if(mid>=j)return query(i,j,x*2);
-    return query(i,mid,x*2)+ query(mid+1,j,x*2+1);
+    if(i>mid)return query(i,j,index*2+1);
+    if(mid>=j)return query(i,j,index*2);
+    return query(i,mid,index*2)+ query(mid+1,j,index*2+1);
 }
 int main(){
     vector<int> nums = {1,10,5,148,78,2,56,231,5,64,65,32,1,8};
     int n  = nums.size();
     segment_tree.resize(n*4);
     build(0,n-1,1,nums);
-    modify(5,1,100);
+    modify(5,1,100);//index always be 1
     // cout << "++++++++++\n";
     for(int i =0;i<n;i++){
         for(int j = i ;j<n;j++){
-            cout << query(i,j,1)<<" ";
+            cout << query(i,j,1)<<" ";//index always be 1
         }cout << endl;
     }
 }
